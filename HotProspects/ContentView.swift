e5c51dd6661e2e988640e0400804b6797398e4f6
Andrei Korikov <kpanic666@gmail.com>
2021-09-20 15:51:25 +0500

    Initial Commit

---
+++ HotProspects/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Andrei Korikov on 20.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Andrei Korikov on 20.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
